a04d016b4bde53234db88d356f4755142e9c54e5
Joey Huang <kamidox@gmail.com>
2014-08-14 22:18:24 +0800

    Update docs

--- envsetup.sh
+++ envsetup.sh
@@ -1,21 +1,9 @@
 # !/bin/bash
 # This script used to setup runing enviroment
 
-# install Flask
-sudo apt-get install python-pip
-sudo pip install Flask
-
-# install scrapy and scrapyd
+# add scrapy source
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 627220E7
 echo 'deb http://archive.scrapy.org/ubuntu scrapy main' | sudo tee /etc/apt/sources.list.d/scrapy.list
 sudo apt-get update
-sudo apt-get install scrapy-0.25 scrapyd
-
-# install mysql and python-mysqldb
-sudo apt-get install mysql-server python-mysqldb
-# setup mysql server and create database, this should do by manual
-# when install mysql-server, we set the password of 'root' as 'kamidox'
-# if you want to change the password, you can use following command:
-# mysqladmin -uroot -pkamidox password 'your new password'
-# mysql -uroot -pkamidox
-# mysql> CREATE DATABASE producthunt
+# install software
+sudo apt-get -y install scrapy-0.25 scrapyd python-pip mysql-server python-mysqldb
