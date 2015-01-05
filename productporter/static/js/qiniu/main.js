fc152ec8971cf1d582ea88e3d93d3144feb5e23b
Joey Huang <kamidox@gmail.com>
2015-01-06 01:15:54 +0800

    change qiniu bucket

--- productporter/static/js/qiniu/main.js
+++ productporter/static/js/qiniu/main.js
@@ -10,7 +10,7 @@ var uploader = Qiniu.uploader({
     // uptoken : '<Your upload token>', //若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
     unique_names: true, // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
     // save_key: true,   // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
-    domain: 'http://7tsycf.com1.z0.glb.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
+    domain: 'http://7u2exe.com1.z0.glb.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
     // container: 'container',           //上传区域DOM ID，默认是browser_button的父元素，
     max_file_size: '100mb',           //最大文件体积限制
     flash_swf_url: 'qiniu/plupload/Moxie.swf',  //引入flash,相对路径
